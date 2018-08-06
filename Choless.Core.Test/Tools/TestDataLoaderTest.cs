5fcd48855e21c7c7a346be4af82be0e8a409fd3c
StarkShang <stark.shang@sjtu.edu.cn>
2018-08-06 11:36:29 +0800

    Add test data loader

---
+++ Choless.Core.Test/Tools/TestDataLoaderTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Choless.Core.Brands;
+using Choless.Core.Tools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Choless.Core.Test.Tools
+{
+    [TestClass]
+    public class TestDataLoaderTest
+    {
+        [TestMethod]
+        public void TestLoadData()
+        {
+            var data = TestDataLoader.LoadData<Brand>();
+            Assert.IsInstanceOfType(data, typeof(IEnumerable<Brand>));
+        }
+
+        [TestMethod]
+        public void TestSaveData()
+        {
+            var headquarters = new Headquarters
+            {
+                HeadquartersId = "",
+                Country = "China",
+                Province = "Shanghai",
+                City = "Shanghai",
+                Address = "No. 800 Dongchuan Road"
+            };
+            var brand = new Brand
+            {
+                BrandId = "00000000000000000000000000000001",
+                BrandName = "Choless",
+                WebSite = "www.choless.com",
+                Headquarters = headquarters,
+                EstablishedTime = new DateTime(2018, 3, 3, 12, 12, 0),
+                Description = "A company to make less choices."
+            };
+            TestDataLoader.SaveData(brand);
+            Assert.Fail();
+        }
+    }
+}
---
+++ Choless.Core/Tools/TestDataLoader.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Choless.Core.Tools
+{
+    public static class TestDataLoader
+    {
+        public const string TestDatabasePath = "Choless.Data/TestData";
+
+        public static IEnumerable<T> LoadData<T>()
+        {
+            Directory.SetCurrentDirectory("/Users/stark/Documents/Projects/Choless");
+            var path = Path.Combine(TestDatabasePath, $"{typeof(T).Name.ToLower()}.json");
+            var fileInfo = new FileInfo(path);
+            if (!fileInfo.Exists)
+                throw new FileNotFoundException(
+                    message: $"Test data file {fileInfo.Name} for not found in {fileInfo.DirectoryName}",
+                    fileName: fileInfo.FullName);
+
+            return JsonConvert.DeserializeObject<IEnumerable<T>>(File.ReadAllText(fileInfo.FullName));
+        }
+
+        public static void SaveData<T>(T obj)
+        {
+            Directory.SetCurrentDirectory("/Users/stark/Documents/Projects/Choless");
+            var path = Path.Combine(TestDatabasePath, $"{typeof(T).Name.ToLower()}.json");
+            var fileInfo = new FileInfo(path);
+            if (!fileInfo.Exists)
+                throw new FileNotFoundException(
+                    message: $"Test data file {fileInfo.Name} for not found in {fileInfo.DirectoryName}",
+                    fileName: fileInfo.FullName);
+            var json = JsonConvert.SerializeObject(obj);
+        }
+
+        public static void SaveData<T>(IEnumerable<T> collection)
+        {
+            foreach (var obj in collection)
+            {
+                Directory.SetCurrentDirectory("/Users/stark/Documents/Projects/Choless");
+                var path = Path.Combine(TestDatabasePath, $"{typeof(T).Name.ToLower()}.json");
+                var fileInfo = new FileInfo(path);
+                if (!fileInfo.Exists)
+                    throw new FileNotFoundException(
+                        message: $"Test data file {fileInfo.Name} for not found in {fileInfo.DirectoryName}",
+                        fileName: fileInfo.FullName);
+                
+            }
+        }
+    }
+}
